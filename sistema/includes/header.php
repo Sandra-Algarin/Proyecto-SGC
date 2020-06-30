9fac68d7cac5e84e8311b99dbeb08d1d9858a12d
Sandra-Algarin <62475332+Sandra-Algarin@users.noreply.github.com>
2020-06-29 21:02:18 -0400

    Add files via upload

---
+++ sistema/includes/header.php
@@ -0,0 +1,26 @@
+<?php 
+
+	if(empty($_SESSION['active']))
+	{
+		header('location: ../');
+	}
+?>
+	<header>
+		<div class="header">			
+			<h1>Despensa Aldito</h1>
+			<div class="optionsBar">
+				<p>San Pedro del Paraná, <?php echo fechaC(); ?></p>
+				<span>|</span>
+				<span class="user"><?php echo $_SESSION['user'].' -'.$_SESSION['rol'].' -'.$_SESSION['email']; ?></span>
+				<img class="photouser" src="img/user.png" alt="Usuario">
+				<a href="salir.php"><img class="close" src="img/salir.png" alt="Salir del sistema" title="Salir"></a>
+			</div>
+		</div>
+<?php include "nav.php"; ?>
+</header>
+ <!-- Clase modal generico para la ventanita de alerta--> 
+ <!-- event.preventDefault() evita q se recargue el formulario --> 
+<div class="modal">
+	<div class="bodyModal">
+	</div>
+</div>
